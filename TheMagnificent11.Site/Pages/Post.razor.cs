96dfb6dd35cf399d5320e52ebc09b4bf28c34010
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-12-30 12:44:22 +1000

    wip

---
+++ TheMagnificent11.Site/Pages/Post.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TheMagnificent11.Site.Pages;
+
+public partial class Post
+{
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+
+    private string Markdown { get; set; } = string.Empty;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        this.Markdown = MarkDownHelper.GetMarkdownFromEmbeddedResource($"Pages.{this.Id}.md");
+    }
+}
